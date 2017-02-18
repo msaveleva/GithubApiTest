19070098020ec46ad103e818c890eec8f2c114d0
Maria Saveleva <mariiasaveleva@gmail.com>
2017-02-18 17:45:25 +0300

    Initial commit

---
+++ GithubApiTest-ObjC/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  GithubApiTest-ObjC
+//
+//  Created by MariaSaveleva on 18/02/2017.
+//  Copyright © 2017 MariaSaveleva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ GithubApiTest-ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubApiTest-ObjC
+//
+//  Created by MariaSaveleva on 18/02/2017.
+//  Copyright © 2017 MariaSaveleva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
